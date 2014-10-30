772fe7ab3062bf74613b977c3d933d4c0764fb7c
Voloshyn Dmytro <wdmytriy@gmail.com>
2014-10-30 22:19:52 +0200

    aded fix for iteration

--- source/statistics.js
+++ source/statistics.js
@@ -8,7 +8,20 @@ var client = require('./utils/redis');
 function statistics(app) {
 	app.route('/api/stats').get(function (req, res) {
       client.hgetall("websites", function (err, obj) {
-         res.json(obj);
+        var counter = {};
+
+        Object.keys(obj).forEach(function(key) {
+           client.get(key, function(err, reply) {
+             counter[key] = reply;
+           });
+        });
+
+        setTimeout(function() {
+          logger.info(counter);
+          res.json(counter);
+        }, 1000);
+
+
       });
 	});
 }
