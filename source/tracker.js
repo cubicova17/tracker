a2b3c23738b0ac95c1c960659cbf359a20a1b8f1
Voloshyn Dmytro <wdmytriy@gmail.com>
2014-10-30 23:34:30 +0200

    dumb website error

--- source/tracker.js
+++ source/tracker.js
@@ -15,7 +15,7 @@ function tracker(app) {
 
 	app.route('/api/track').get(validate, function (req, res, next) {
     client.incr(req.query.partner);
-    client.incr(website + moment().utc().toDate().toString().substring(0, 10)); //write to website+date key e.g.: "preply.com10-09-2014"
+    client.incr(req.query.partner + moment().utc().toDate().toString().substring(0, 10)); //write to website+date key e.g.: "preply.com10-09-2014"
     client.hsetnx("websites", req.query.partner, true);
     return res.send(200);
 	});
