264a39652d8566a3d4e06d78d2cfa59fc0218fd4
Voloshyn Dmytro <wdmytriy@gmail.com>
2014-10-30 23:31:40 +0200

    async

--- source/statistics.js
+++ source/statistics.js
@@ -1,28 +1,29 @@
-var _ = require('underscore');
-var moment = require('moment');
-
-var config = require('../config');
+var async = require('async');
 var logger = require('./utils/logger');
 var client = require('./utils/redis');
 
 function statistics(app) {
-	app.route('/api/stats').get(function (req, res) {
-      client.hgetall("websites", function (err, obj) {
-        var counter = {};
+	app.route('/api/stats').get(function (req, res, next) {
+		client.hgetall("websites", function (err, websites) {
+			var getCounterByUrl = function (counter, url, callback) {
+				client.get(url, function (err, count) {
+					if (err) {
+						return callback(err);
+					}
 
-        Object.keys(obj).forEach(function(key) {
-           client.get(key, function(err, reply) {
-             counter[key] = reply;
-           });
-        });
+					counter[url] = count;
+					callback(null, counter);
+				});
+			};
 
-        setTimeout(function() {
-          logger.info(counter);
-          res.json(counter);
-        }, 1000);
+			async.reduce(Object.keys(websites), {}, getCounterByUrl, function (err, counter) {
+				if (err) {
+					return next(err);
+				}
 
-
-      });
+				res.json(counter);
+			});
+		});
 	});
 }
 
